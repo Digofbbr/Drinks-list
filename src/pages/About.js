52a5eb4f98dd1ce2fd886c547047999e56dd8088
Rodrigo Barreto <rodrigofb06@gmail.com>
2022-09-30 13:38:14 -0300

    pronto

---
+++ src/pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>About us</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ut arcu euismod, pulvinar metus pretium, ullamcorper nulla.
+        Sed ullamcorper, massa sit amet tincidunt tempor, augue orci ullamcorper metus, sed dictum felis tortor ultricies ex. Vestibulum mi est, pharetra ac interdum.
+      </p>
+    </section>
+  )
+}
+
+export default About
